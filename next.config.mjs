cd93e3d7fb4e5d3c2937b56c0656325dffb4a455
Tesseract <136876204+ntesseract@users.noreply.github.com>
2024-09-10 10:44:29 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+// export default nextConfig;
+// Above this was the original ones
+const nextConfig = {
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
 
-export default nextConfig;
+module.exports = nextConfig;
